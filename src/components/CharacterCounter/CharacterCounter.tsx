fecbdc7d21a1afec85941536549329f3cd119139
Shanosha <wootenm1@gmail.com>
2026-02-18 12:29:45 -0500

    feature/implemented responsive tailwind classes

--- src/components/CharacterCounter/CharacterCounter.tsx
+++ src/components/CharacterCounter/CharacterCounter.tsx
@@ -42,9 +42,11 @@ function CharacterCounter ({minWords = 25, maxWords = 100, targetReadingTime = 0
 
     return (
 
-        <div className="w-xs mx-auto my-8 flex flex-col">
+        <div className="flex justify-center">
+        <div className="min-w-4/5 mx-4 my-8 flex flex-col">
             <TextInput onTextChange={handleTextChange} />
             <StatsDisplay stats={stats} showReadingTime={true} />
+        </div>
         </div>
 
     )
--- src/components/StatsDisplay/StatsDisplay.tsx
+++ src/components/StatsDisplay/StatsDisplay.tsx
@@ -23,19 +23,19 @@ function StatsDisplay ({stats, showReadingTime = true}: StatsDisplayProps) {
 
     return (
 
-        <div className="w-full flex flex-row gap-4 justify-around text-center text-sm mt-4 p-4 border border-gray-300 rounded-lg">
-            <div className="flex flex-col gap-2">
-                <p>Characters</p>
-                <div className="text-2xl font-medium">{stats.characterCount}</div>
+        <div className="w-full flex sm:flex-row flex-col gap-1 justify-around text-center text-sm mt-4 border border-gray-300 rounded-lg">
+            <div className="flex flex-col gap-1 p-4 sm:border-0 border-b border-b-gray-300">
+                <p className="text-gray-600">Characters</p>
+                <div className="text-3xl font-medium">{stats.characterCount}</div>
             </div>
-            <div className="flex flex-col gap-2">
-                <p>Words</p>
-                <div className={validWordCount(stats.wordCount,stats.minWords,stats.maxWords)?"text-green-600 text-2xl font-medium":"text-red-600 text-2xl font-medium"}>{stats.wordCount}</div>
-                <p>Min: {stats.minWords} | Max: {stats.maxWords}</p>
+            <div className="flex flex-col gap-1 p-4 sm:border-0 border-b border-b-gray-300">
+                <p className="text-gray-600">Words</p>
+                <div className={validWordCount(stats.wordCount,stats.minWords,stats.maxWords)?"text-green-600 text-3xl font-medium":"text-red-600 text-3xl font-medium"}>{stats.wordCount}</div>
+                <p className="text-gray-600 text-xs">Min: {stats.minWords} | Max: {stats.maxWords}</p>
             </div>
-            <div className="flex flex-col gap-2">
-                <p>Reading Time</p>
-                {showReadingTime && (<div className="text-2xl font-medium">{secondsToMMSS(stats.readingTime)}</div>)}
+            <div className="flex flex-col gap-1 p-4">
+                <p className="text-gray-600">Reading Time</p>
+                {showReadingTime && (<div className="text-3xl font-medium">{secondsToMMSS(stats.readingTime)}</div>)}
             </div>
         </div>
 
